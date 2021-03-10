aa7563bfa6e97686489651aca3c368c1111df433
LI <ayalilian2018@gmail.com>
2021-03-09 23:20:04 -0300

    see

--- tdvr/src/main/resources/sql/album-insert.sql
+++ tdvr/src/main/resources/sql/album-insert.sql
@@ -1,5 +1,11 @@
-INSERT INTO
-Album(id, title, released, Artist_id)
-
-(1, 'Like a Prayer', '1988-01-01 00:00:00', 1),
-(2, 'Like a Virgin', '1984-01-01 00:00:00', 1);
+INSERT INTO Album
+    (id, title, released, Artist_id)
+VALUES
+    (1, 'Like a Virgin', '1984-01-01 00:00:00', 1),
+    (2, 'Like a Prayer', '1988-01-01 00:00:00', 1),
+    (3, 'Baby One More Time', '1999-01-01 00:00:00', 2),
+    (4, 'Oops!... I Did It Again', '2000-01-01 00:00:00', 2),
+    (5, 'The Fame', '2008-01-01 00:00:00', 3),
+    (6, 'Born This Way', '2011-01-01 00:00:00', 3),
+    (7, 'One of the Boys', '2008-01-01 00:00:00', 4),
+    (8, 'Teenage Dream', '2010-01-01 00:00:00', 4);
